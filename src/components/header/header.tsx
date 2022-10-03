e6f0367723c4de548cb0be658dd1f151eb4a9790
Алексей <thekomix@gmail.com>
2022-10-04 00:18:54 +0500

    мелкие исправления

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -2,21 +2,16 @@ import * as React from 'react';
 import headerStyles from './header.module.css';
 import {BurgerIcon, ListIcon, Logo, ProfileIcon} from "@ya.praktikum/react-developer-burger-ui-components";
 
-export interface HeaderProps {
-    className?: string
-    children: React.ReactNode
-}
-
 export function Header() {
     return (
         <div className={headerStyles.header}>
             <div className={headerStyles.content}>
                 <div className={headerStyles.navigation}>
-                    <a className={`${headerStyles.menuItem} ${headerStyles.menuItemActive} text text_type_main-default`} href="#">
+                    <a className={`${headerStyles.menuItem} ${headerStyles.menuItemActive} text text_type_main-default pl-4 pr-4 pt-5 pb-5 `} href="#">
                         <BurgerIcon type="primary"/>
                         Конструктор
                     </a>
-                    <a className={`${headerStyles.menuItem} text text_type_main-default`} href="#">
+                    <a className={`${headerStyles.menuItem} text text_type_main-default pl-4 pr-4 pt-5 pb-5 `} href="#">
                         <ListIcon type="secondary"/>
                         Лента заказов
                     </a>
@@ -25,7 +20,7 @@ export function Header() {
                     <Logo/>
                 </div>
                 <div className={headerStyles.menuProfile}>
-                    <a className={`${headerStyles.menuItem} text text_type_main-default`} href="#">
+                    <a className={`${headerStyles.menuItem} text text_type_main-default pl-4 pr-4 pt-5 pb-5 `} href="#">
                         <ProfileIcon type="secondary"/>
                         Личный кабинет
                     </a>
