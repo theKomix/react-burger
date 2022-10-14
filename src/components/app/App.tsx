10961698c7b7d8ce4dc2e49eca2efddb394e4cdc
Алексей <thekomix@gmail.com>
2022-10-14 11:08:04 +0500

    Добавлен контекст и вызов makeorder

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -6,31 +6,94 @@ import {BurgerIngredientList} from "../burger-ingredients/burger-ingredient-list
 import {BurgerConstructor} from "../burger-constructor/burger-constructor";
 import {GetIngredients} from "../../services/get-ingredients";
 import {Modal} from "../modal/modal";
+import {
+    InitialOrderState,
+    Order,
+    OrderAction,
+    OrderActionType,
+    OrderContext,
+    OrderContextState
+} from "../../context/order-context";
+
+export const orderReducer = (state: Order, action: OrderAction): Order => {
+    const calcOrderSum = (ingredients: Ingredient[]) => {
+        return ingredients.reduce(
+            (partialSum, a) => (
+                partialSum + a.price
+            ), 0);
+    };
+
+    switch (action.type) {
+        case OrderActionType.ADD: {
+            let newIngredients = [...state.ingredients, action.payload];
+            return {
+                ...state,
+                sum: calcOrderSum(newIngredients),
+                ingredients: newIngredients
+            }
+        }
+        case OrderActionType.REMOVE: {
+            let newIngredients = state.ingredients.filter(i => i !== action.payload);
+            return {
+                ...state,
+                sum: calcOrderSum(newIngredients),
+                ingredients: [...newIngredients]
+            }
+        }
+        case OrderActionType.SET: {
+            return {
+                ...state,
+                sum: calcOrderSum(action.payload),
+                ingredients: [...action.payload]
+            }
+        }
+        case OrderActionType.MAKE: {
+            return {
+                ...state,
+                number: action.payload
+            }
+        }
+        case OrderActionType.ERROR: {
+            return {
+                ...state,
+                error: action.payload
+            }
+        }
+        default:
+            throw new Error('Unexpected action');
+    }
+};
 
 function App() {
     const [listIngredients, setListIngredients] = React.useState<Ingredient[]>([]);
-    const [topBun, setTopBun] = React.useState<Ingredient | null>(null);
-    const [bottomBun, setBottomBun] = React.useState<Ingredient | null>(null);
-    const [selectedIngredients, setSelectedIngredients] = React.useState<Ingredient[]>([]);
-    const [error, setError] = React.useState<string>("");
+    const [orderState, changeOrderState] = React.useReducer<React.Reducer<Order, OrderAction>>(orderReducer, InitialOrderState);
+    const OrderContextState: OrderContextState = {
+        state: orderState,
+        changeState: changeOrderState
+    };
 
     React.useEffect(() => {
         GetIngredients()
             .then(items => {
                 setListIngredients(items);
-                setTopBun(items.find(i => i.type === "bun") ?? null);
-                setBottomBun(items.find(i => i.type === "bun") ?? null);
 
                 //заполнение для теста
+                let bun = items.find(i => i.type === "bun")!;
                 const notBunIngs = items.filter(i => (i.type !== "bun"));
-                const selected = [] as Ingredient[];
-                for (let i = 0; i < 7; i++) {
-                    selected.push(notBunIngs[Math.floor(Math.random() * notBunIngs.length)]);
-                }
-                setSelectedIngredients(selected);
+                changeOrderState({
+                    type: OrderActionType.SET,
+                    payload: [
+                        bun,
+                        bun,
+                        ...Array.from(Array(7).keys()).map(
+                            () => notBunIngs[Math.floor(Math.random() * notBunIngs.length)])]
+                });
             })
             .catch(() => {
-                setError("Произошла ошибка, перезагрузите страницу...")
+                changeOrderState({
+                    type: OrderActionType.ERROR,
+                    payload: "Произошла ошибка, перезагрузите страницу..."
+                });
             });
     }, []);
 
@@ -39,9 +102,11 @@ function App() {
     return (
         <div className="App">
             <Header/>
-            <BurgerIngredientList list={listIngredients} selected={[topBun, bottomBun, ...selectedIngredients]}/>
-            <BurgerConstructor topBun={topBun} bottomBun={bottomBun} list={selectedIngredients}/>
-            {error && <Modal onClose={onErrorClose} header={"Ошибка"}><div className="mt-5 text text_type_main-default">{error}</div></Modal>}
+            <OrderContext.Provider value={OrderContextState}>
+                <BurgerIngredientList list={listIngredients} />
+                <BurgerConstructor />
+            </OrderContext.Provider>
+            {orderState.error && <Modal onClose={onErrorClose} header={"Ошибка"}><div className="mt-5 text text_type_main-default">{orderState.error}</div></Modal>}
         </div>
     );
 }
--- src/services/api-urls.ts
+++ src/services/api-urls.ts
@@ -1,1 +1,2 @@
 export const GetIngredientsUrl = "https://norma.nomoreparties.space/api/ingredients";
+export const PostOrderUrl = "https://norma.nomoreparties.space/api/orders";
