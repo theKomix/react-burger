58daf80b50ed1e343e28be570e3292f6ea76e427
Алексей <thekomix@gmail.com>
2022-12-14 09:54:56 +0500

    Объект выделен, убран повторяющийся код

--- src/services/order-feed/order-feed.test.ts
+++ src/services/order-feed/order-feed.test.ts
@@ -7,11 +7,17 @@ import reducer, {
     close,
     error,
     message,
-    WebsocketStatus
+    WebsocketStatus, OrderFeedState
 } from "./order-feed-slice";
 import {OrderList} from "../../models/order-list";
 
 describe('order feed tests', () => {
+    const testState: OrderFeedState = {
+        orderList: {orders: [], total: 0, totalToday: 0},
+        status: WebsocketStatus.OFFLINE,
+        connectionError: ""
+    };
+
     it('should return the initial state', () => {
         expect(reducer(undefined, {type: undefined})).toEqual(
             initialState
@@ -20,41 +26,34 @@ describe('order feed tests', () => {
 
     it("connect", () => {
         expect(reducer({
-            orderList: {orders: [], total: 0, totalToday: 0},
-            status: WebsocketStatus.OFFLINE,
+            ...testState,
             connectionError: "ERROR"
         }, connect({url: "", token: ""}))).toEqual(
-            {orderList: {orders: [], total: 0, totalToday: 0}, status: WebsocketStatus.CONNECTING, connectionError: ""}
+            {...testState, status: WebsocketStatus.CONNECTING}
         );
     });
 
     it("connecting", () => {
-        expect(reducer({
-            orderList: {orders: [], total: 0, totalToday: 0},
-            status: WebsocketStatus.OFFLINE,
-            connectionError: ""
-        }, connecting())).toEqual(
-            {orderList: {orders: [], total: 0, totalToday: 0}, status: WebsocketStatus.CONNECTING, connectionError: ""}
+        expect(reducer({...testState}, connecting())).toEqual(
+            {...testState, status: WebsocketStatus.CONNECTING}
         );
     });
 
     it("disconnect", () => {
         expect(reducer({
-            orderList: {orders: [], total: 0, totalToday: 0},
-            status: WebsocketStatus.ONLINE,
-            connectionError: ""
+            ...testState,
+            status: WebsocketStatus.ONLINE
         }, disconnect())).toEqual(
-            {orderList: {orders: [], total: 0, totalToday: 0}, status: WebsocketStatus.OFFLINE, connectionError: ""}
+            {...testState}
         );
     });
 
     it("connected", () => {
         expect(reducer({
-            orderList: {orders: [], total: 0, totalToday: 0},
-            status: WebsocketStatus.CONNECTING,
-            connectionError: ""
+            ...testState,
+            status: WebsocketStatus.CONNECTING
         }, connected())).toEqual(
-            {orderList: {orders: [], total: 0, totalToday: 0}, status: WebsocketStatus.ONLINE, connectionError: ""}
+            {...testState, status: WebsocketStatus.ONLINE}
         );
     });
 
@@ -69,45 +68,47 @@ describe('order feed tests', () => {
                         ingredients: [],
                         createdAt: "",
                         updateAt: ""
-                    }], total: 1, totalToday: 1
-                }, status: WebsocketStatus.ONLINE, connectionError: ""
+                    }],
+                    total: 1,
+                    totalToday: 1
+                },
+                status: WebsocketStatus.ONLINE,
+                connectionError: ""
             },
             close()))
             .toEqual({
-                    orderList: {orders: [], total: 0, totalToday: 0},
-                    status: WebsocketStatus.OFFLINE,
-                    connectionError: ""
+                    ...testState
                 }
             );
     });
 
     it("error", () => {
         expect(reducer({
-            orderList: {orders: [], total: 0, totalToday: 0},
-            status: WebsocketStatus.ONLINE,
-            connectionError: ""
+            ...testState,
+            status: WebsocketStatus.ONLINE
         }, error("ERROR"))).toEqual(
             {
-                orderList: {orders: [], total: 0, totalToday: 0},
-                status: WebsocketStatus.OFFLINE,
+                ...testState,
                 connectionError: "ERROR"
             }
         );
     });
 
     const orderList: OrderList = {
-        success: true, message: "",
+        success: true,
+        message: "",
         orders: [
             {_id: "1", name: "", status: "done", number: 1, ingredients: [], createdAt: "1", updateAt: ""},
             {_id: "2", name: "", status: "done", number: 2, ingredients: [], createdAt: "2", updateAt: ""}
-        ], total: 1, totalToday: 1
+        ],
+        total: 1,
+        totalToday: 1
     };
 
     it("message", () => {
         expect(reducer({
-                orderList: {orders: [], total: 0, totalToday: 0},
-                status: WebsocketStatus.ONLINE,
-                connectionError: ""
+                ...testState,
+                status: WebsocketStatus.ONLINE
             },
             message(JSON.stringify(orderList)))).toEqual(
             {
@@ -118,7 +119,8 @@ describe('order feed tests', () => {
                         orderList.orders[0]
                     ]
                 },
-                status: WebsocketStatus.ONLINE, connectionError: ""
+                status: WebsocketStatus.ONLINE,
+                connectionError: ""
             }
         );
     });
